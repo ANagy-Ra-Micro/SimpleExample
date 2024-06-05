02de1663bc5a28a72d0122f41448c3d403dae725
Attila Nagy <ottiatze@gmx.de>
2024-06-05 08:44:15 +0200

    'initial'

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
